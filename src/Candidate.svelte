4fe49b6b462c80801b811aaa95c9013cd37b9c5c
williamhsieh <wh31110@gmail.com>
2022-07-23 17:55:02 +0800

    [FEAT] implement pencil-box

---
+++ src/Candidate.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let cell_id;
+</script>
+
+<div class="cell">
+  {#each Array(3) as _, row}
+    {#each Array(3) as _, col}
+      <div class="candidate">
+        {row * 3 + col + 1}
+      </div>
+    {/each}
+  {/each}
+</div>
+
+<style>
+  .candidate {
+    font-size: 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .cell {
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    grid-auto-rows: auto;
+  }
+</style>
--- src/Cell.svelte
+++ src/Cell.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Candidate from './Candidate.svelte';
   import { onMount } from 'svelte';
   import { puzzle, focusedCellId, conflictCell, shouldCheck } from './store'
 
@@ -8,10 +9,11 @@
   const prefilled = $puzzle[x][y] != 0;
   let style = "cell";
 
+  $: filled = $puzzle[x][y] != 0;
   $: conflicted = $conflictCell[x][y];
   $: focused = $focusedCellId == cell_id;
-  $: focusedNumber = $focusedCellId != -1
-    && $focusedCellId != cell_id
+  $: focusedNumber = !focused
+    && $focusedCellId != -1
     && $puzzle[x][y] != 0
     && $puzzle[Math.floor($focusedCellId / 9)][$focusedCellId % 9] == $puzzle[x][y];
 
@@ -37,48 +39,39 @@
 
 <svelte:window on:keydown={handleKeydown}/>
 
-<div class:prefilled class:focusedNumber class:focused class="{style}" on:click={handleClick}>
+<div class:filled class:prefilled class:focusedNumber class:focused class={style} on:click={handleClick}>
   {#if $puzzle[x][y] != 0}
     {$puzzle[x][y]}
+    <div class:conflicted></div>
+  {:else}
+    <Candidate cell_id={cell_id} />
   {/if}
-  <div class:conflicted></div>
 </div>
 
 <style>
   .cell {
-    position: relative;
-    width: 40px;
-    height: 40px;
+    width: 51px;
+    height: 51px;
     font-size: 30px;
 
+    border-top: 1px solid black;
+    border-left: 1px solid black;
+    font-weight: bold;
+  }
+
+  .filled {
     display: flex;
     justify-content: center;
     align-items: center;
-
-    padding: 3px;
-    border-top: 1px solid black;
-    border-left: 1px solid black;
   }
 
-  .top-wall {
-    border-top: 3px solid black;
-  }
-
-  .bottom-wall {
-    border-bottom: 3px solid black;
-  }
-
-  .left-wall {
-    border-left: 3px solid black;
-  }
-
-  .right-wall {
-    border-right: 3px solid black;
-  }
+  .top-wall { border-top: 3px solid black; }
+  .bottom-wall { border-bottom: 3px solid black; }
+  .left-wall { border-left: 3px solid black; }
+  .right-wall { border-right: 3px solid black; }
 
   .prefilled {
     background: #e6e6e6;
-    font-weight: bold;
   }
 
   .focused {
